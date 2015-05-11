e060de43c740e87ee1c322dbe37d7ef72d959247
Angela Matchan <am26@sanger.ac.uk>
2015-05-11 10:52:29 +0100

    Update Tophat.pm

--- perl/lib/Sanger/CGP/Tophat.pm
+++ perl/lib/Sanger/CGP/Tophat.pm
@@ -7,7 +7,7 @@ package Sanger::CGP::Tophat;
 ###
 #This file is part of cgpRna.
 ###
-#TopHatFusion is free software: you can redistribute it and/or modify it under
+#cgpRna is free software: you can redistribute it and/or modify it under
 #the terms of the GNU Affero General Public License as published by the
 #Free Software Foundation; either version 3 of the License, or (at your
 #option) any later version.
