b87ab987df614f390914dfa288984e30023403d7
am26 <am26@sanger.ac.uk>
2015-05-22 10:37:53 +0100

    Changing name of logs directory so there is no clash when three algorithms are run simultaneously and changing tophat_fusion.pl so it will not exit if valid fatsqs are input and process bamtofastq is run

--- perl/bin/defuse.pl
+++ perl/bin/defuse.pl
@@ -95,7 +95,7 @@ sub cleanup {
 	my $options = shift;
 	my $tmpdir = $options->{'tmp'};
 	Sanger::CGP::Defuse::Implement::compress_sam($options);
-	move(File::Spec->catdir($tmpdir, 'logs'), File::Spec->catdir($options->{'outdir'}, 'logs')) || die $!;
+	move(File::Spec->catdir($tmpdir, 'logs'), File::Spec->catdir($options->{'outdir'}, 'logs_defuse')) || die $!;
 	remove_tree $tmpdir if(-e $tmpdir);
 	return 0;
 }
