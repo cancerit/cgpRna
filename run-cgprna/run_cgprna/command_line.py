0341b9e5c9622bee2bed6fa0a0f1b43a542eddad
Yaobo Xu <thelastsheep@hotmail.com>
2019-10-29 16:55:55 +0000

    added CLI to run infuse tools

--- run-cgprna/run_cgprna/command_line.py
+++ run-cgprna/run_cgprna/command_line.py
@@ -10,6 +10,7 @@ from .map import map_seq_files
 from .mapping_stats import generate_stats
 from .htseq_count import count
 from .bigwig import generate_bigwig
+from .fusion_tools import tophat_fusion, star_fusion, defuse
 
 version = pkg_resources.require("run_cgprna")[0].version
 
@@ -52,22 +53,22 @@ def main():
     parser_a.add_argument(
         '-sp', '--species', dest='species',
         metavar='STR',
-        help='Species name. No need to set if using a pre-built reference bundle. If using a folder as the reference, it\' be used to locate reference files.',
+        help='Species name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it\' be used to locate reference files.',
         required=False)
     parser_a.add_argument(
         '-rb', '--reference-build', dest='ref_build',
         metavar='STR',
-        help='Reference build name. No need to set if using a pre-built reference bundle. If using a folder as the reference, it\' be used to locate reference files.',
+        help='Reference build name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it\' be used to locate reference files.',
         required=False)
     parser_a.add_argument(
         '-gb', '--gene-build', dest='gene_build',
         metavar='STR',
-        help='Gene build name. No need to set if using a pre-built reference bundle. If using a folder as the reference, it\' be used to locate for GTF file.',
+        help='Gene build name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it\' be used to locate for GTF file.',
         required=False)
     parser_a.add_argument(
         '-gtf', '--gene-build-gtf-name', dest='gene_build_gtf_name',
         metavar='STR',
-        help='File name of the gene build file. No need to set if using a pre-built reference bundle. If using a folder as the reference, it\' be used to locate the GTF file.',
+        help='File name of the gene build annotaion file. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it\' be used to locate the GTF file.',
         required=False)
     parser_a.add_argument(
         '-od', '--output-directory', dest='out_dir',
@@ -111,7 +112,7 @@ def main():
         required=False)
     parser_a.set_defaults(func=map_seq_files)
 
-    # create the parser for the "stats" command
+    # create the parser for "stats" command
     parser_b = subparsers.add_parser(
         'stats',
         parents=[common_parser],
@@ -138,7 +139,7 @@ def main():
         required=False)
     parser_b.set_defaults(func=generate_stats)
 
-    # create the parser for the "bigwig" command
+    # create the parser for "bigwig" command
     parser_c = subparsers.add_parser(
         'bigwig',
         parents=[common_parser],
@@ -151,7 +152,7 @@ def main():
     parser_c.add_argument(
         '-r', '--reference', dest='ref',
         metavar='FASTA_FILE',
-        help='FASTA file of a reference file, which the input BAM file was mapped to.',
+        help='FASTA file of a reference, which the input BAM file was mapped to.',
         required=True)
     parser_c.add_argument(
         '-od', '--output-directory', dest='out_dir',
@@ -165,7 +166,7 @@ def main():
         required=False)
     parser_c.set_defaults(func=generate_bigwig)
 
-    # create the parser for the "count" command
+    # create the parser for "count" command
     parser_d = subparsers.add_parser(
         'count',
         parents=[common_parser],
@@ -186,6 +187,151 @@ def main():
         help='Output directory. Default: current directory.',
         required=False)
     parser_d.set_defaults(func=count)
+
+    # create the parser for "tophat_fusion" command
+    parser_e = subparsers.add_parser(
+        'tophat-fusion',
+        parents=[common_parser],
+        description='Use Tophat2 to identify gene fusion events.')
+    parser_e.add_argument(
+        '-i', '--input', dest='input',
+        metavar='FILE',
+        nargs='+',
+        help='Input files, can be BAM files or FastQ files or a mixture of both. File names of FastQ files much have suffix of "_1" or "_2" immediately prior to ".f[ast]q".',
+        required=True)
+    parser_e.add_argument(
+        '-s', '--sample-name', dest='sample_name',
+        metavar='STR',
+        help='Sample name, which will used to prefix output file names and SM tag in the BAM file header.',
+        required=True)
+    parser_e.add_argument(
+        '-r', '--reference', dest='ref',
+        metavar='TAR|PATH',
+        help='A reference bundle tar file or the path to reference root directory.',
+        required=True)
+    parser_e.add_argument(
+        '-od', '--output-directory', dest='out_dir',
+        metavar='DIR', default='.',
+        help='Output directory. Default: current directory.',
+        required=False)
+    parser_e.add_argument(
+        '-sp', '--species', dest='species',
+        metavar='STR',
+        help='Species name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should be the name of an existing folder containing one or more folders named with differenct reference builds in \'--reference\' folder.',
+        required=False)
+    parser_e.add_argument(
+        '-rb', '--reference-build', dest='ref_build',
+        metavar='STR',
+        help='Reference build name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should the name of an existing folder containing \'tophat\' folder for this reference build in \'--species\' folder.',
+        required=False)
+    parser_e.add_argument(
+        '-gb', '--gene-build', dest='gene_build',
+        metavar='STR',
+        help='Gene build name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should be the name of an existing folder containing transcriptome index files in \'tophat\' folder.',
+        required=False)
+    parser_e.add_argument(
+        '-t', '--threads', dest='threads',
+        metavar='INT', type=int, default=1,
+        help='Number of threads to use.',
+        required=False)
+    parser_e.set_defaults(func=tophat_fusion)
+
+    # create the parser for "star_fusion" command
+    parser_f = subparsers.add_parser(
+        'star-fusion',
+        parents=[common_parser],
+        description='Use STAR to identify gene fusion events.')
+    parser_f.add_argument(
+        '-i', '--input', dest='input',
+        metavar='FILE',
+        nargs='+',
+        help='Input files, can be BAM files or FastQ files or a mixture of both. File names of FastQ files much have suffix of "_1" or "_2" immediately prior to ".f[ast]q".',
+        required=True)
+    parser_f.add_argument(
+        '-s', '--sample-name', dest='sample_name',
+        metavar='STR',
+        help='Sample name, which will used to prefix output file names and SM tag in the BAM file header.',
+        required=True)
+    parser_f.add_argument(
+        '-r', '--reference', dest='ref',
+        metavar='TAR|PATH',
+        help='A reference bundle tar file or the path to reference root directory.',
+        required=True)
+    parser_f.add_argument(
+        '-od', '--output-directory', dest='out_dir',
+        metavar='DIR', default='.',
+        help='Output directory. Default: current directory.',
+        required=False)
+    parser_f.add_argument(
+        '-sp', '--species', dest='species',
+        metavar='STR',
+        help='Species name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should be the name of an existing folder containing one or more folders named with differenct reference builds in \'--reference\' folder.',
+        required=False)
+    parser_f.add_argument(
+        '-rb', '--reference-build', dest='ref_build',
+        metavar='STR',
+        help='Reference build name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should the name of an existing folder containing \'star\' folder for this reference build in \'--species\' folder.',
+        required=False)
+    parser_f.add_argument(
+        '-gb', '--gene-build', dest='gene_build',
+        metavar='STR',
+        help='Gene build name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should be the name of an existing folder containing a GTF file in \'star\' folder.',
+        required=False)
+    parser_f.add_argument(
+        '-t', '--threads', dest='threads',
+        metavar='INT', type=int, default=1,
+        help='Number of threads to use.',
+        required=False)
+    parser_f.set_defaults(func=star_fusion)
+
+
+    # create the parser for "defuse" command
+    parser_g = subparsers.add_parser(
+        'defuse',
+        parents=[common_parser],
+        description='Use Defuse to identify gene fusion events.')
+    parser_g.add_argument(
+        '-i', '--input', dest='input',
+        metavar='FILE',
+        nargs='+',
+        help='Input files, can be BAM files or FastQ files or a mixture of both. File names of FastQ files much have suffix of "_1" or "_2" immediately prior to ".f[ast]q".',
+        required=True)
+    parser_g.add_argument(
+        '-s', '--sample-name', dest='sample_name',
+        metavar='STR',
+        help='Sample name, which will used to prefix output file names and SM tag in the BAM file header.',
+        required=True)
+    parser_g.add_argument(
+        '-r', '--reference', dest='ref',
+        metavar='TAR|PATH',
+        help='A reference bundle tar file or the path to reference root directory.',
+        required=True)
+    parser_g.add_argument(
+        '-od', '--output-directory', dest='out_dir',
+        metavar='DIR', default='.',
+        help='Output directory. Default: current directory.',
+        required=False)
+    parser_g.add_argument(
+        '-sp', '--species', dest='species',
+        metavar='STR',
+        help='Species name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should be the name of an existing folder containing one or more folders named with differenct reference builds in \'--reference\' folder.',
+        required=False)
+    parser_g.add_argument(
+        '-rb', '--reference-build', dest='ref_build',
+        metavar='STR',
+        help='Reference build name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should the name of an existing folder containing \'defuse\' folder for this reference build in \'--species\' folder.',
+        required=False)
+    parser_g.add_argument(
+        '-gb', '--gene-build', dest='gene_build',
+        metavar='STR',
+        help='Gene build name. No need to set if using a pre-built reference bundle. If using a folder path as the value of \'--reference\', it should be the name of an existing folder containing all Defuse index files in \'defuse\' folder.',
+        required=False)
+    parser_g.add_argument(
+        '-t', '--threads', dest='threads',
+        metavar='INT', type=int, default=1,
+        help='Number of threads to use.',
+        required=False)
+    parser_g.set_defaults(func=defuse)
 
     args = parser.parse_args()
     if len(sys.argv) > 1:
---
+++ run-cgprna/run_cgprna/fusion_tools.py
@@ -0,0 +1,173 @@
+import os
+import sys
+import re
+import shutil
+import copy
+from string import Template
+from . import run_templates_in_shell, untar, mkdir
+import gzip
+
+BAM_TO_FASTQ = Template('bamtofastq exclude=SECONDARY,SUPPLEMENTARY T=$bam2fq_tmp S=$bam2fq_tmp_single_end O=$bam2fq_tmp_unmatched O2=$bam2fq_tmp_unmatched2 gz=1 level=1 F=$bam2fq_tmp_matched F2=$bam2fq_tmp_matched_2 filename=$in_bam')
+# Defuse and its wapper defuse_fusion.pl cannot handle gzipped file.
+BAM_TO_FASTQ_FOR_DEFUSE = Template('bamtofastq exclude=SECONDARY,SUPPLEMENTARY T=$bam2fq_tmp S=$bam2fq_tmp_single_end O=$bam2fq_tmp_unmatched O2=$bam2fq_tmp_unmatched2 F=$bam2fq_tmp_matched F2=$bam2fq_tmp_matched_2 filename=$in_bam')
+TOPHAT_FUSION = Template('tophat_fusion.pl -s $sample_name -o $out_dir -t $threads -r $reference_data_root -sp $species -rb $ref_build -gb $gene_build $input')
+STAR_FUSION = Template('star_fusion.pl -s $sample_name -o $out_dir -t $threads -r $reference_data_root -sp $species -rb $ref_build -gb $gene_build $input')
+DEFUSE_FUSION = Template('defuse_fusion.pl -s $sample_name -o $out_dir -t $threads -r $reference_data_root -sp $species -rb $ref_build -gb $gene_build $input')
+DEFUSE_FILTER = Template('defuse_filters.pl -s $sample_name -o $out_dir -i $out_dir/${sample_name}.defuse-fusion.normals.filtered.txt')
+
+NORMAL_FUSION_ARG_NAME='-normals'
+
+REF_RELATED_DEFAULTS = {
+    'species': 'unspecified_species',
+    'ref_build': 'unspecified_ref_build',
+    'gene_build': 'ensembl'
+}
+
+
+def validate_input_seq_files(file_names):
+    '''
+    Perl wrappers: tophat_fusion.pl, star_fusion.pl and defuse.pl all support multiple input files but are restricted into a single type, i.e either BAM or FastQ files. As we're converting BAMs to fastqs before passing them to the wappers, we can easily handle a mixture of BAM and FastQ files, but needs to validate the files before starting to convert BAM to FastQ, so we don't waste time and resources on splitting a BAM when some of the FastQ files are not following the Perl wrapper's file name conventions.
+    '''
+    pairs = {}
+    for a_file in file_names:
+        if a_file.endswith('.bam'):
+            continue
+        if not os.path.exists(a_file):
+            sys.exit('Error: can not find input file: %s' % a_file)
+        a_file = os.path.abspath(a_file)
+        match = re.match(r'(.*)_([12])\.f(?:ast)?q(?:\.gz)?$', os.path.basename(a_file))
+        if match.group(2):
+            # if same mate number been spotted before or already got both mates
+            if pairs.get(match.group(1), 0) == int(match.group(2)) or pairs.get(match.group(1), 0) == 3:
+                sys.exit('Error: Too many \'_%s\' mate files for prefix: %s. Possibly redundant file: %s' % (match.group(2), match.group(1), a_file))
+            pairs[match.group(1)] = pairs.get(match.group(1), 0) + int(match.group(2))
+        else:
+            sys.exit('Error: File name does not follow expected coventions: %s' % a_file)
+    for a_mate, value in pairs.items():
+        # if both _1 and _2 of a pair of fastq files are give, value should be 3
+        if value != 3:
+            if value == 1:
+                sys.exit('Error: Can not find second mate file of: %s' % a_mate)
+            if value == 2:
+                sys.exit('Error: Can not find first mate file of: %s' % a_mate)
+            sys.exit('Error: Internal code logic error')
+
+
+
+# NOTE: So far only used if Defuse is given a gzipped fastq file
+def gunzip(source_file, dest_file):
+    print('unzipping %s ...' % os.path.basename(source_file), flush=True)
+    with gzip.open(source_file, 'rb') as s_file, open(dest_file, 'wb') as d_file:
+        shutil.copyfileobj(s_file, d_file, 65536)
+    print('done.', flush=True)
+
+
+def run_fusion_wrapper(args, temp_dir_name, fusion_templates, no_gzip=False):
+    # args to dict to allow updates later
+    args_dict = copy.deepcopy(vars(args))
+    # only use temp_dir when needed to extract reference files
+    temp_dir = os.path.join(os.path.abspath(args.out_dir), temp_dir_name)
+    mkdir(temp_dir)
+
+    # valide inputs before bamtofastq, otherwise it could be to late
+    validate_input_seq_files(args.input)
+
+    # prepare the output dir
+    mkdir(args.out_dir)
+
+    reference_data_root=os.path.abspath(args.ref)
+
+    # Anything not a file will be treated as a reference root folder
+    if not os.path.isfile(reference_data_root):
+        if not os.path.exists(reference_data_root):
+            sys.exit('Error: cound not locate directory: %s' % reference_data_root)
+        if any(args_dict[ele] is None for ele in REF_RELATED_DEFAULTS.keys()):
+            sys.exit(
+                'Error: missing required input. When "--reference" is not a reference bundle tar file, you have to provide: %s' % ', '.join(
+                    [ '--' + key.replace('_', '-') for key in REF_RELATED_DEFAULTS.keys() if args_dict[key] is None])
+            )
+    elif not os.path.basename(reference_data_root).endswith('.tar.gz'):
+        # check if input ref file has valid file extensions
+        sys.exit('Error: wrong input format. "--reference" can only be a tar.gz file or a folder.')
+    else:
+        reference_data_root=os.path.join(temp_dir, 'ref')
+        # set ref related args to defaults if not given
+        for arg_name in REF_RELATED_DEFAULTS.keys():
+            if args_dict[arg_name] is None:
+                print('Set "%s" to default.' % arg_name)
+                args_dict[arg_name] = REF_RELATED_DEFAULTS[arg_name]
+            if arg_name == 'gene_build':
+                print('Make sure a folder named "%s" exists in the ref bundle.' % args_dict[arg_name])
+        untar(args.ref, os.path.join(reference_data_root, args_dict['species'], args_dict['ref_build']))
+
+    input_fastqs = []
+    fq_lane_name_count = 1
+
+    # dumb Defuse perl wrapper doesn't like gzipped, so: 
+    fq_suffix, bam_to_fq_template = ('fq.gz', BAM_TO_FASTQ) if not no_gzip else ('fq', BAM_TO_FASTQ_FOR_DEFUSE)
+
+    for a_raw_file in args.input:
+        # if input is not a bam, assume it's a fastq. i.e not supporing CRAM.
+        if not a_raw_file.endswith('.bam'):
+            if no_gzip and a_raw_file.endswith('.gz'):
+                # dumb Defuse perl wrapper doesn't like gzipped, so:
+                unzip_to = os.path.join(temp_dir, re.sub(r'\.gz$', '', os.path.basename(a_raw_file)))
+                gunzip(a_raw_file, unzip_to)
+                input_fastqs.append(os.path.abspath(unzip_to))
+            else:
+                input_fastqs.append(os.path.abspath(a_raw_file))
+            continue
+
+        bam2fq_params = {
+            'bam2fq_tmp': os.path.join(temp_dir, '%s.%s' % (args.sample_name, fq_lane_name_count)),
+            'bam2fq_tmp_single_end': os.path.join(temp_dir, '%s.%s.s' % (args.sample_name, fq_lane_name_count)),
+            'bam2fq_tmp_unmatched': os.path.join(temp_dir, '%s.%s.o1' % (args.sample_name, fq_lane_name_count)),
+            'bam2fq_tmp_unmatched2': os.path.join(temp_dir, '%s.%s.o2' % (args.sample_name, fq_lane_name_count)),
+            'bam2fq_tmp_matched': os.path.join(temp_dir, '%s.%s_1.%s' % (args.sample_name, fq_lane_name_count, fq_suffix)),
+            'bam2fq_tmp_matched_2': os.path.join(temp_dir, '%s.%s_2.%s' % (args.sample_name, fq_lane_name_count, fq_suffix)),
+            'in_bam': os.path.abspath(a_raw_file)
+        }
+        run_templates_in_shell([bam_to_fq_template], bam2fq_params)
+        input_fastqs.extend([
+            bam2fq_params['bam2fq_tmp_matched'],
+            bam2fq_params['bam2fq_tmp_matched_2']
+        ])
+        fq_lane_name_count += 1
+
+    # gathering parameters
+    params = {
+        'sample_name': args.sample_name,
+        'input': ' '.join(input_fastqs),
+        'out_dir': os.path.abspath(args.out_dir),
+        'threads': args.threads,
+        'reference_data_root': reference_data_root,
+        'species': args_dict['species'],
+        'ref_build': args_dict['ref_build'],
+        'gene_build': args_dict['gene_build']
+    }
+
+    run_templates_in_shell(fusion_templates, params)
+
+    # clean temp dir
+    shutil.rmtree(temp_dir)
+
+
+def tophat_fusion(args):
+    '''
+    Top level entry point for running tophat_fusion on RNA-Seq data.
+    '''
+    run_fusion_wrapper(args, 'cgpRna_tophat-fusion_temp', [TOPHAT_FUSION])
+
+
+def star_fusion(args):
+    '''
+    Top level entry point for running tophat_fusion on RNA-Seq data.
+    '''
+    run_fusion_wrapper(args, 'cgpRna_star-fusion_temp', [STAR_FUSION])
+
+
+def defuse(args):
+    '''
+    Top level entry point for running tophat_fusion on RNA-Seq data.
+    '''
+    run_fusion_wrapper(args, 'cgpRna_defuse_temp', [DEFUSE_FUSION, DEFUSE_FILTER], True)
