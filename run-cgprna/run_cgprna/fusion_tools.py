6d568ba9a2104040d94ea67d85104e073f7b81fa
Yaobo Xu <thelastsheep@hotmail.com>
2019-11-06 16:48:18 +0000

    improved code of fusion_tools.py as lb29 suggested in pull request #39

--- run-cgprna/run_cgprna/fusion_tools.py
+++ run-cgprna/run_cgprna/fusion_tools.py
@@ -29,29 +29,30 @@ def validate_input_seq_files(file_names):
     Perl wrappers: tophat_fusion.pl, star_fusion.pl and defuse.pl all support multiple input files but are restricted into a single type, i.e either BAM or FastQ files. As we're converting BAMs to fastqs before passing them to the wappers, we can easily handle a mixture of BAM and FastQ files, but needs to validate the files before starting to convert BAM to FastQ, so we don't waste time and resources on splitting a BAM when some of the FastQ files are not following the Perl wrapper's file name conventions.
     '''
     pairs = {}
+    fq_name_pattern = re.compile(r'(.*)_([12])\.f(?:ast)?q(?:\.gz)?$')
     for a_file in file_names:
         if a_file.endswith('.bam'):
             continue
         if not os.path.exists(a_file):
             sys.exit('Error: can not find input file: %s' % a_file)
         a_file = os.path.abspath(a_file)
-        match = re.match(r'(.*)_([12])\.f(?:ast)?q(?:\.gz)?$', os.path.basename(a_file))
+        match = fq_name_pattern.match(os.path.basename(a_file))
         if match.group(2):
             # if same mate number been spotted before or already got both mates
-            if pairs.get(match.group(1), 0) == int(match.group(2)) or pairs.get(match.group(1), 0) == 3:
-                sys.exit('Error: Too many \'_%s\' mate files for prefix: %s. Possibly redundant file: %s' % (match.group(2), match.group(1), a_file))
-            pairs[match.group(1)] = pairs.get(match.group(1), 0) + int(match.group(2))
+            pair_name, first_or_second_mate_in_a_pair = match.groups()
+            if pairs.get(pair_name, 0) == int(first_or_second_mate_in_a_pair) or pairs.get(pair_name, 0) == 3:
+                sys.exit('Error: Too many \'_%s\' mate files for prefix: %s. Possibly redundant file: %s' % (first_or_second_mate_in_a_pair, pair_name, a_file))
+            pairs[pair_name] = pairs.get(pair_name, 0) + int(first_or_second_mate_in_a_pair)
         else:
             sys.exit('Error: File name does not follow expected coventions: %s' % a_file)
-    for a_mate, value in pairs.items():
-        # if both _1 and _2 of a pair of fastq files are give, value should be 3
-        if value != 3:
-            if value == 1:
-                sys.exit('Error: Can not find second mate file of: %s' % a_mate)
-            if value == 2:
-                sys.exit('Error: Can not find first mate file of: %s' % a_mate)
-            sys.exit('Error: Internal code logic error')
-
+    for pair_name, sum_of_mate_numbers in pairs.items():
+        # if both _1 and _2 of a pair of fastq files are given, value should be 3
+        if sum_of_mate_numbers != 3:
+            if sum_of_mate_numbers == 1:
+                sys.exit('Error: Can not find second mate file of: %s' % pair_name)
+            if sum_of_mate_numbers == 2:
+                sys.exit('Error: Can not find first mate file of: %s' % pair_name)
+            sys.exit('Error: Internal code logic error, sum of first and second mate number in a pair of fastq files is %d, which is not expected and not handled well in code.' % sum_of_mate_numbers)
 
 
 # NOTE: So far only used if Defuse is given a gzipped fastq file
@@ -107,11 +108,16 @@ def run_fusion_wrapper(args, temp_dir_name, fusion_templates, no_gzip=False):
     fq_suffix, bam_to_fq_template = ('fq.gz', BAM_TO_FASTQ) if not no_gzip else ('fq', BAM_TO_FASTQ_FOR_DEFUSE)
 
     for a_raw_file in args.input:
-        # if input is not a bam, assume it's a fastq. i.e not supporing CRAM.
+        if a_raw_file.endswith('.cram'):
+            sys.exit('Error: CRAM file is not supported, please remove %s from input and retry.' % a_raw_file)
+        # if input is not a bam, assume it's a fastq.
         if not a_raw_file.endswith('.bam'):
             if no_gzip and a_raw_file.endswith('.gz'):
                 # dumb Defuse perl wrapper doesn't like gzipped, so:
-                unzip_to = os.path.join(temp_dir, re.sub(r'\.gz$', '', os.path.basename(a_raw_file)))
+                unzip_to = os.path.join(
+                    temp_dir,
+                    os.path.basename(a_raw_file)[:-3]  # remove the last 3 chars from the base name, which should be '.gz' 
+                )
                 gunzip(a_raw_file, unzip_to)
                 input_fastqs.append(os.path.abspath(unzip_to))
             else:
@@ -128,10 +134,10 @@ def run_fusion_wrapper(args, temp_dir_name, fusion_templates, no_gzip=False):
             'in_bam': os.path.abspath(a_raw_file)
         }
         run_templates_in_shell([bam_to_fq_template], bam2fq_params)
-        input_fastqs.extend([
+        input_fastqs += [
             bam2fq_params['bam2fq_tmp_matched'],
             bam2fq_params['bam2fq_tmp_matched_2']
-        ])
+        ]
         fq_lane_name_count += 1
 
     # gathering parameters
